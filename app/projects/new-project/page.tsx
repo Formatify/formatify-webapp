5cf5cef4d85cae6bb081db6cac8754a8736c73fe
Usama <uusman004@gmail.com>
2024-05-28 08:25:17 +0500

    Navbar and Stepper Done

--- app/projects/new-project/page.tsx
+++ app/projects/new-project/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import StepperFrom from './components/StepperFrom'
 
 export default function page() {
   return (
-    <div>New Project</div>
+    <div className='flex justify-center items-center bg-slate-200 w-full h-[90vh]'><StepperFrom /></div>
   )
 }
