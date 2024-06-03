6e5aaaac1ce7d051b3d5208fb19081c190dd98ac
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2024-06-03 23:30:59 +0500

    chore: Update NEXTAUTH_URL for production deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     env: {
-        NEXTAUTH_URL: isProd ? 'https://formatify.vercel.app/' : 'http://localhost:3000/',
+        NEXTAUTH_URL: isProd ? 'https://formatify.onrender.com/' : 'http://localhost:3000/',
     },
     images: {
         domains: ['images.unsplash.com'],
