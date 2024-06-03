8e6cff20b587ee626dcf1419cbdbba269828e980
Affan <affanatweb@gmail.com>
2024-06-03 16:29:35 +0500

    Feat: Coming Soon Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,59 @@
+import Image from "next/image";
+import { LinkedinnIcon,formatifyLogo, FbIcon, XIcon, InstIcon } from "@/utils/constant";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl max-[500px]:text-2xl">Home Page Formatify</h1>
-    </main>
+    <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.05] relative flex items-center justify-center flex-col">
+      {/* Radial gradient for the container to give a faded look */}
+      <Image width={75} height={75} src={formatifyLogo} alt="Logo" className="mt-12 z-10"></Image>
+      <div className="flex flex-col items-center justify-center mt-10">
+        <div className=" text-4xl font-bold  lg:text-8xl"> FORMATIFY </div>
+
+        <div className="flex justify-center items-end gap-8 ">
+          <div className=" text-4xl font-bold  lg:text-8xl"> WEBSITE IS </div>
+          <div className=" w-80 md:text-xs lg:text-base lg:font-medium lg:mb-4 hidden md:block lg:block">
+            {" "}
+            Subscribe to be the first to know about our website and product
+            updates!{" "}
+          </div>
+        </div>
+
+        <div className="text-4xl font-bold lg:text-8xl"> ON THE WAY </div>
+
+       <div className="flex justify-center items-center "> 
+       <div className=" text-center text-xs w-72 mt-6 lg:hidden md:hidden ">
+            {" "}
+            Subscribe to be the first to know about our website and product
+            updates!{" "}
+          </div>
+       </div>
+        
+      </div>
+
+      <div className="flex justify-center items-center gap-4 mt-6">
+        <input className=" rounded-3xl w-48 text-xs lg:text-base md:text-base md:w-96 lg:w-96 " placeholder="Please enter your e-mail address" />
+        <button className="bg-green-500 text-white text-xs w-20 md:text-base lg:text-base md:w-32 lg:w-32 rounded-3xl border h-9 lg:h-11 md:h-11 " >Subscribe</button>
+      </div>
+
+      <div className="">
+        <ul className="flex gap-6">
+          <li className="z-10">
+            <Link href={""}> <Image width={40} height={40} src={LinkedinnIcon} alt="Logo" className="mt-12"></Image> </Link>
+          </li>
+          <li className="z-10">
+            <Link href={""}> <Image width={40} height={40} src={InstIcon} alt="Logo" className="mt-12"></Image> </Link>
+          </li>
+          <li className="z-10">
+            <Link href={""}> <Image width={40} height={40} src={FbIcon} alt="Logo" className="mt-12"></Image> </Link>
+          </li>
+          <li className="z-10">
+            <Link href={""}> <Image width={40} height={40} src={XIcon} alt="Logo" className="mt-12"></Image> </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,25 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const svgToDataUri = require("mini-svg-data-uri");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 
 const config: Config = {
   content: [
@@ -6,6 +27,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +37,23 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+    require("@tailwindcss/forms"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="70" height="70" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+         
+         
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
 export default config;
