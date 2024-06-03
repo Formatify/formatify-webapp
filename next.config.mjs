7ce30785992d790686638000353c0cf5b6161062
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2024-06-03 22:46:47 +0500

    chore: Update environment variables for production deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+    env: {
+        NEXTAUTH_URL: isProd ? 'https://formatify.vercel.app/' : 'http://localhost:3000/',
+    },
     images: {
         domains: ['images.unsplash.com'],
     },
